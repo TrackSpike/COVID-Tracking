29bfa4a259343d3c158565b77c6c32c8e9aacc6e
Justin Andrews <47611641+JustinAndrewsCS@users.noreply.github.com>
2020-12-09 08:44:01 -0500

    Uploaded ios folder

---
+++ Final Product/Source Code/covid_app/covid_app/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<file_picker/FilePickerPlugin.h>)
+#import <file_picker/FilePickerPlugin.h>
+#else
+@import file_picker;
+#endif
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
+#import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FilePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FilePickerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
