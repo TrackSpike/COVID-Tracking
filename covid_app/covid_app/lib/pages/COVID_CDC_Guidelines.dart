6707349da6bfd1fd675ebfd33ccc3da659ec294e
Bryce Cole <brycewcole@gmail.com>
2021-02-25 14:50:41 -0500

    Updated Teja's code

--- covid_app/covid_app/lib/pages/COVID_CDC_Guidelines.dart
+++ covid_app/covid_app/lib/pages/COVID_CDC_Guidelines.dart
@@ -11,21 +11,29 @@ class _COVID_CDC_GuidelinesState extends State<COVID_CDC_Guidelines> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('CDC Guidelines'),
-        ),body: Center(
+      appBar: AppBar(
+        title: Text('CDC Guidelines'),
+      ),
+      body: Center(
         child: Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: [
-      Center(child: Text("Wash and Sanitize your Hands", style: TextStyle(fontSize: 100))),
-      Center(child: Text("Stay Six feet from people outside your home", style: TextStyle(fontSize: 100))),
-      Center(child: Text("Wear mask when in public places and avoid gatherings", style: TextStyle(fontSize: 100))),
-    ],),),),);
-
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Center(
+                  child: Text("Wash and Sanitize your Hands",
+                      style: TextStyle(fontSize: 15))),
+              Center(
+                  child: Text("Stay Six feet from people outside your home",
+                      style: TextStyle(fontSize: 15))),
+              Center(
+                  child: Text(
+                      "Wear mask when in public places and avoid gatherings",
+                      style: TextStyle(fontSize: 15))),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
-
