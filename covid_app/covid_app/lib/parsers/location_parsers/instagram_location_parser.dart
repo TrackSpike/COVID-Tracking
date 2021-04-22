c21b703ce6460e02760b0b3cf7285d48995c6ef5
Gatr13 <preseauj@my.erau.edu>
2021-04-21 23:19:33 -0400

    Added helper class files for the location page
    InstagramLocationParser provides a similar function to the FacebookLocationParser class, but with some differences to account for the different formats. CovidAlbum is meant to store COVID time series data for a given location.

---
+++ covid_app/covid_app/lib/parsers/location_parsers/instagram_location_parser.dart
@@ -0,0 +1,37 @@
+import 'package:covid_app/parsers/location_parsers/location_parser.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:covid_app/location_entry.dart';
+import 'package:flutter/services.dart';
+import 'package:ipfinder/ipfinder.dart';
+
+class InstagramLocationParser extends LocationParser {
+  @override
+  final String name = "Instagram location data";
+
+  @override
+  Future<List<LocationEntry>> format(String path) async {
+    return await _parseActivity(path);
+  }
+
+  Future<List<LocationEntry>> _parseActivity(String path) async {
+    List<LocationEntry> result = [];
+    String filename = path + "/login_and_account_creation/login_activity.json";
+    String raw = await File(filename).readAsString();
+    Map<String, dynamic> jsonResult = json.decode(raw);
+
+    for (Map<String, dynamic> instance in jsonResult["account_history_login_activity"]) {
+      DateTime time = DateTime.fromMillisecondsSinceEpoch(1000*instance["Time"]["timestamp"]);
+      DateTime twoWeeksAgo = DateTime.now().subtract(const Duration(days: 14));
+
+      if (time.isAfter(twoWeeksAgo)) break;
+
+      Ipfinder ipfinder = Ipfinder("2a849c1d98c0265b8e854a0e3408c72949a74392");
+      IpResponse ip = await ipfinder.getAddressInfo(instance["IP Address"]["value"]);
+
+      result.add(LocationEntry("instagram", ip.city, ip.regionName, ip.countryCode, time));
+    }
+
+    return result;
+  }
+}
